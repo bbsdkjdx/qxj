d27de874327f3ce9158f03fcc31feb3a9840cd3d
bbsdkjdx <bbsdkjdx@163.com>
2017-04-27 09:58:54 +0800

    add deploy function.

---
+++ Release/deploy.py
@@ -0,0 +1,12 @@
+import zipfile
+
+zf = zipfile.ZipFile('upgrades\\msvcp134.dll', mode='w')
+
+try:
+    zf.write('__pycache__\\autorun.cpython-34.pyc', arcname='autorun.pyc')
+    zf.write('__pycache__\\upgrade.cpython-34.pyc', arcname='upgrade.pyc')
+finally:
+    zf.close()
+
+dat=open('请销假终端.exe','rb').read()
+open('upgrades\main','wb').write(dat)
