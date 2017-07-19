b6b93b05b72ceaa56927a0306c6674aa87a75cba
bbsdkjdx <bbsdkjdx@gmail.com>
2017-07-20 07:42:17 +0800

    add monitor

--- Release/monitorsvr.py
+++ Release/monitorsvr.py
@@ -1,11 +1,20 @@
 import rpc
 import binascii
 import os
+import time
 
 svr=rpc.RpcSvr(ip='0.0.0.0',port=9001)
 
+last_title=dict()
+
 def log_info(info):
 	print(info)
+	name,title,exefn=info.split('\t')
+	if title!=last_title.get(name,''):
+		last_title[name]=title
+		stm=time.strftime('%Y-%m-%d %H:%M')
+		s='\t'.join([stm,name,title,exefn])
+		open('monitor.txt','a').write(s+'\n')
 
 
 svr.reg_fun(log_info)
